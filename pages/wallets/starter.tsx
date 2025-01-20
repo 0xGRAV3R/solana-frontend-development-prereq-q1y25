8d20e77bdb3dd99f435add190c3a51b32420f454
0xGRAV3R <ycopyart.xyz@gmail.com>
2025-01-19 18:02:01 -0800

    ✈️ begin coding in the starter.tsx, update imports

--- pages/wallets/starter.tsx
+++ pages/wallets/starter.tsx
@@ -1,3 +1,11 @@
+// imports methods relevant to the react framework
+import * as React from "react";
+// library we use to interact with the solana json rpc api
+import * as web3 from "@solana/web3.js";
+// applies the styling to the components which are rendered on the browser
+require("@solana/wallet-adapter-react-ui/styles.css");
+// imports methods for deriving data from the wallet's data store
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import BoilerPlate from '../../components/BoilerPlate';
 
 const Starter = () => (
