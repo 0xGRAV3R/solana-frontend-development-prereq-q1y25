573f64f85c7c112b308f608dca0d45c542d19af9
0xGRAV3R <ycopyart.xyz@gmail.com>
2025-01-19 20:39:03 -0800

    ✈️ Add imports for the counter section

--- pages/counter/starter.tsx
+++ pages/counter/starter.tsx
@@ -1,3 +1,13 @@
+import React, { useState, useEffect } from "react";
+import * as web3 from "@solana/web3.js";
+import { toast } from "react-toastify";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { ExternalLinkIcon } from "@heroicons/react/outline";
+import { Program, AnchorProvider } from "@coral-xyz/anchor";
+import NodeWallet from "@coral-xyz/anchor/dist/cjs/nodewallet";
+import CounterIDL from "../../programs/idls/counter.json";
+import { Counter } from "../../programs/types/counter";
+import { Keypair, PublicKey } from "@solana/web3.js";
 import BoilerPlate from '../../components/BoilerPlate';
 
 const Starter = () => (
