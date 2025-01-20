741753165c3649769018bdd3f42564a396dcef2b
0xGRAV3R <ycopyart.xyz@gmail.com>
2025-01-19 20:10:19 -0800

    ✈️ Add the handling of transaction, signing it, and sending it to the recipient

--- pages/sendsol/starter.tsx
+++ pages/sendsol/starter.tsx
@@ -13,7 +13,47 @@ const Starter = () => {
   
     const { connection } = useConnection();
     const { publicKey, sendTransaction } = useWallet();
+
+    const handleTransaction = async () => {
+        if (!connection || !publicKey) {
+          toast.error("Please connect your wallet.");
+          return;
+        }
+    
+        const { blockhash, lastValidBlockHeight } =
+          await connection.getLatestBlockhash();
+        const txInfo = {
+          /** The transaction fee payer */
+          feePayer: publicKey,
+          /** A recent blockhash */
+          blockhash: blockhash,
+          /** the last block chain can advance to before tx is exportd expired */
+          lastValidBlockHeight: lastValidBlockHeight,
+        };
+        const transaction = new web3.Transaction(txInfo);
+        const instruction = web3.SystemProgram.transfer({
+          fromPubkey: publicKey,
+          lamports: amount * web3.LAMPORTS_PER_SOL,
+          toPubkey: new web3.PublicKey(account),
+        });
+    
+        transaction.add(instruction);
+    
+        try {
+          const signature = await sendTransaction(transaction, connection);
+          setTxSig(signature);
+    
+          const newBalance = balance - amount;
+          setBalance(newBalance);
+        } catch (error) {
+          console.log(error);
+          toast.error("Transaction failed!");
+        }
+    }; 
+
     return <BoilerPlate />;
   };
+
+ 
 
 export default Starter;
