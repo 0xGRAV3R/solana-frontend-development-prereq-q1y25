8feac780d61bcc140b396da508d008d0c7f2bb7f
0xGRAV3R <ycopyart.xyz@gmail.com>
2025-01-19 19:56:25 -0800

    ✈️ Add the imports for the sendsol section

--- pages/sendsol/starter.tsx
+++ pages/sendsol/starter.tsx
@@ -1,3 +1,8 @@
+import React, { useState, useEffect } from "react";
+import * as web3 from "@solana/web3.js";
+import { toast } from "react-toastify";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { ExternalLinkIcon } from "@heroicons/react/outline";
 import BoilerPlate from '../../components/BoilerPlate';
 
 const Starter = () => (
